eee1ade5c8d6185d45677b507745061f71ce9703
Thanusan <thanukuga@gmail.com>
2020-03-19 13:42:42 +0100

    update comments

--- src/occgrid_planner_base/src/occgrid_planner.cpp
+++ src/occgrid_planner_base/src/occgrid_planner.cpp
@@ -228,7 +228,7 @@ class OccupancyGridPlanner {
             // element is always the closer to the start.
             Heap heap;
             heap.insert(Heap::value_type(heuristic(start,target), start)); //  Heap::value_type(0, start)
-            while (!heap.empty()) {
+            while (!heap.empty()) {                                                                                 
                 // Select the cell at the top of the heap
                 Heap::iterator hit = heap.begin();
                 // the cell it contains is this_cell
